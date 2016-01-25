1e09d66bfa2291d21c15fb654760c52c2e88c4c1
stinaskold <Stina@Stinas-iMac.lan>
2016-01-25 19:56:08 +0100

    Skapade org-loggedin.html, org.js, ändringar i main.css

---
+++ js/org.js
@@ -0,0 +1,49 @@
+
+
+$("#show-messages").click(function() {
+  $("#org-form-div").empty();
+  var html = '<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">' +
+             '<ul class="org-messages">' +
+              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></li>' +
+              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></li>' +
+              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></li>' +
+             '</ul>' +
+      '</div>';
+  $("#org-message-div").html(html);
+});
+
+$("#create-message").click(function() {
+  $("#org-message-div").empty();
+  var html = '<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">' +
+               '<ul>' +
+                 '<li><label for="" style="color: white;">Field</label><br>' +
+                 '<input type="text" class="field"></li>' +
+                 '<li><label for="" style="color: white;">Country</label><br>' +
+                 '<input type="text" class="country"></li>' +
+                 '<li><label for="" style="color: white;">Header</label><br>' +
+                 '<input type="text" class="header"></li>' +
+                 '<li class="textarea-message"><label for="" style="color: white;">Message</label><br>' +
+                 '<textarea class="message" rows="5"></textarea></li>' +
+                 '<li><button class="btn btn-lg btn-default" id="send-message">Send message</button></li>' +
+               '</ul>' +
+             '<div class="messages"></div>' +
+             '</div>';
+  $("#org-form-div").html(html);
+
+  document.querySelector("#send-message").addEventListener("click",function(){
+      $("#org-form-div").html("<h3 class='message-sent col-xs-12 col-sm-12 col-md-6 col-md-offset-3'>Your message was sent.</h3>");
+
+      var field, country, header, message;
+      field = document.querySelector(".field").value;
+      country = document.querySelector(".country").value;
+      header = document.querySelector(".header").value;
+      message = document.querySelector(".message").value;
+
+      addMissionToDatabase(field, country, header, message);
+  });
+});
+
+function addMissionToDatabase(field, country, header, message){
+    myFireBaseRef.child('missioninfo').push({field: field, country: country, header: header, message: message});
+    window.location.reload();
+}
