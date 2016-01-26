89520e5669c3ef7220eaf34e7cde47b5fce6c2f2
stinaskold <Stina@Stinas-iMac.lan>
2016-01-26 16:04:48 +0100

    changes to org-start, org-logged in, users.js, main.css

--- js/org.js
+++ js/org.js
@@ -4,9 +4,9 @@ $("#show-messages").click(function() {
   $("#org-form-div").empty();
   var html = '<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">' +
              '<ul class="org-messages">' +
-              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></li>' +
-              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></li>' +
-              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></li>' +
+              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><div class="edit-delete"><button class="edit btn btn-default btn-sm ">Edit</button><button class="delete btn btn-default btn-sm">Delete</button></div></li>' +
+              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><div class="edit-delete"><button class="edit btn btn-default btn-sm">Edit</button><button class="delete btn btn-default btn-sm">Delete</button></div></li>' +
+              '<li class="panel"><h2>Message headline</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><div class="edit-delete"><button class="edit btn btn-default btn-sm">Edit</button><button class="delete btn btn-default btn-sm">Delete</button></div></li>' +
              '</ul>' +
       '</div>';
   $("#org-message-div").html(html);
@@ -16,6 +16,10 @@ $("#create-message").click(function() {
   $("#org-message-div").empty();
   var html = '<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">' +
                '<ul>' +
+                 '<li><label for="" style="color: white;">Organisation</label><br>' +
+                 '<input type="text" class="organisation"></li>' +
+                 '<li><label for="" style="color: white;">Email</label><br>' +
+                 '<input type="text" class="email"></li>' +
                  '<li><label for="" style="color: white;">Field</label><br>' +
                  '<input type="text" class="field"></li>' +
                  '<li><label for="" style="color: white;">Country</label><br>' +
@@ -31,8 +35,6 @@ $("#create-message").click(function() {
   $("#org-form-div").html(html);
 
   document.querySelector("#send-message").addEventListener("click",function(){
-      //$("#org-form-div").html("<h3 class='message-sent col-xs-12 col-sm-12 col-md-6 col-md-offset-3'>Your message was sent.</h3>");
-
       var field, country, header, message;
       field = document.querySelector(".field").value;
       country = document.querySelector(".country").value;
