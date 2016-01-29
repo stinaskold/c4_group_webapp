e9ec9d7ff1c06a28a7e28a673155c966644a62ca
stinaskold <Stina@Stinas-iMac.lan>
2016-01-29 21:09:01 +0100

    kommenterat bort mina ändringar

--- js/chat.js
+++ js/chat.js
@@ -2,9 +2,9 @@
 /////////////////
 ///Stinas kod
 
-    var usernameRef = new Firebase('https://fro15-c4-webapp.firebaseio.com/users/username');
-    //var username = usersRef.child("username").toString();
-    var test = usernameRef.child("username").toString();
+    // var usernameRef = new Firebase('https://fro15-c4-webapp.firebaseio.com/users/username');
+    // //var username = usersRef.child("username").toString();
+    // var test = usernameRef.child("username").toString();
   //   console.log(test);
   // usernameRef.on("value", function(snapshot) {
   // console.log(snapshot.val());
